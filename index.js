06d2fb78306e2123b1c158529452bf0193ab2c48
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-01-07 15:30:50 +0530

    .gitignore

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ async function runChat() {
   try {
     const response = await ollama.chat({
       model: "llama3.2:latest",
-      messages: [{ role: 'user', content: "Generate marketing emails" }]
+      messages: [{ role: 'user', content: "Generate a resignation letter" }]
     });
 
     console.log("Chatbot Response:", response.message.content);

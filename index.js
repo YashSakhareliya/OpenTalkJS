4195e04b55177fec231caa18e460052281185dff
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-01-08 10:03:05 +0530

    V0.0.2

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
+import fs from 'fs';
 import ollama from "ollama";
 
+
+let question = fs.readFileSync('q1.txt').toString()
+console.log(question)
+let answer;
+
+runChat()
 async function runChat() {
-  try {
-    const response = await ollama.chat({
-      model: "llama3.2:latest",
-      messages: [{ role: 'user', content: "Generate a resignation letter" }]
-    });
-
-    console.log("Chatbot Response:", response.message.content);
-  } catch (error) {
-    console.error("Error occurred:", error.message);
+    try {
+      const response = await ollama.chat({
+        model: "llama3.2:latest",
+        messages: [{ role: 'user', content: question }]
+      });
+  
+      answer = response.message.content;
+      console.log("Chatbot Response:", answer);
+      fs.appendFileSync('a1.txt',answer)
+    } catch (error) {
+      console.error("Error occurred:", error.message);
+    }
   }
-}
 
-runChat();

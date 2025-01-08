f36483d4725039d838ce5ae23abb03b7cc4291c8
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-01-08 11:01:12 +0530

    V0.0.3

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
 import fs from 'fs';
-import ollama from "ollama";
+import ollama from 'ollama';
+
+let folder = 'questions';
+
+async function runChat(question) {
+  try {
+    const response = await ollama.chat({
+      model: "llama3.2:latest",
+      messages: [{ role: 'user', content: question }]
+    });
+
+    return response.message.content;
+  } catch (error) {
+    console.error("Error occurred:", error.message);
+  }
+}
 
 
-let question = fs.readFileSync('q1.txt').toString()
-console.log(question)
-let answer;
-
-runChat()
-async function runChat() {
-    try {
-      const response = await ollama.chat({
-        model: "llama3.2:latest",
-        messages: [{ role: 'user', content: question }]
-      });
-  
-      answer = response.message.content;
-      console.log("Chatbot Response:", answer);
-      fs.appendFileSync('a1.txt',answer)
-    } catch (error) {
-      console.error("Error occurred:", error.message);
-    }
+fs.readdir(folder, (err, files) => {
+  if (err) {
+    return console.error('Error reading directory:', err.message);
   }
 
+  files.forEach((file) => {
+    const filepath = `${folder}/${file}`;
+    fs.readFile(filepath, 'utf8', async (err, question) => {
+      if (err) {
+        return console.error(`Error reading file ${file}:`, err.message);
+      }
+      let ans_file = file
+      const response = await runChat(question);
+      console.log(`Question from ${file}:`, question);
+      console.log(`Response:`, response);
+      fs.mkdirSync('answers', { recursive: true })
+      let answer_path = `answers/${ans_file.replace('q','a')}`
+      console.log(`Answer path:`, answer_path)
+      fs.appendFileSync(answer_path, response)
+    });
+  });
+});
